1942b5cb021bc4aab01aeeb485243fd5d0f03c23
Amey Nagmode <ameynagmode@gmail.com>
2021-06-29 22:13:53 +0530

    Add files via upload

---
+++ src/components/About/About.jsx
@@ -0,0 +1,71 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="About Me" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+              <div className="about-wrapper__image">
+                <AboutImg alt="profile picture" filename={img} />
+              </div>
+            </Fade>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne || 'I am currently graduating with bachelor\'s degree in Computer Engineering at GCOE , Jalgaon and actively looking for Internship opportunities \. Modules I am studying during my degree include object-oriented programming , software engineering , software project management, operating systems , database management systems , web programming , computer networks , design and analysis of algorithms and cyber security \. '}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphTwo ||
+                    'My natural curiosity for computers is what drove me towards my computer engineering degree \. I’ve always been fascinated by computers and the technical mechanics behind the monitor I am now eager to secure roles that demand use of the skills gained during my studies'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree || 'I love to connect with new people and build new relationships, so feel free to send me a message..'}
+                </p>
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume}
+                    >
+                      Resume
+                    </a>
+                  </span>
+                )}
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,97 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const Skills = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="skills">
+      <Container>
+        <Title title="Skills" />
+        <Row className="skills-wrapper">
+          <Col md={4} sm={4}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+            <div className="skills-wrapper__info"> 
+            <p className="skills-wrapper__info-text">
+                <img height="40" width="40" src="https://cdn.icon-icons.com/icons2/1488/PNG/512/5352-html5_102567.png" alt="html" /> {paragraphOne || 'HTML5'}
+                </p>
+              </div>
+            </Fade>
+          </Col>
+          <Col md={4} sm={4}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="skills-wrapper__info">
+                <p className="skills-wrapper__info-text">
+                <img height="30" width="30" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Devicon-css3-plain.svg/1200px-Devicon-css3-plain.svg.png" alt="css3logo" /> {paragraphOne || 'CSS3'}
+                </p>
+              </div>
+            </Fade>
+          </Col>
+          <Col md={4} sm={4}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+            <div className="skills-wrapper__info">
+            <p className="skills-wrapper__info-text">
+                <img height="35" width="50" src="https://thumbs.dreamstime.com/b/javascript-logo-javascript-logo-white-background-vector-format-available-136765881.jpg" alt="js" /> {paragraphOne || 'JS'}
+                </p>
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+       
+        <Row className="skills-wrapper">
+          <Col md={4} sm={4}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+            <div className="skills-wrapper__info">
+            <p className="skills-wrapper__info-text">
+                <img height="40" width="30" src="https://image.pngaaa.com/940/2196940-small.png" alt="php" /> {paragraphOne || 'PHP'}
+                </p>
+              </div>
+            </Fade>
+          </Col>
+          <Col md={4} sm={4}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="skills-wrapper__info">
+              <p className="skills-wrapper__info-text">
+                <img height="60" width="60" src="https://download.logo.wine/logo/MySQL/MySQL-Logo.wine.png" alt="mysql" /> {paragraphOne || 'MYSQL'}
+                </p>
+              </div>
+            </Fade>
+          </Col>
+          <Col md={4} sm={4}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+            <div className="skills-wrapper__info">
+            <p className="skills-wrapper__info-text">
+                <img height="35" width="40" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png" alt="bootstrap" /> {paragraphOne || 'BOOTSTRAP'}
+                </p>
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+       
+      </Container>
+    </section>
+  );
+};
+
+
+export default Skills;
+
+
